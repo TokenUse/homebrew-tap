f28995c6994a49c03468ea7b3eb896d11c93c732
Melvyn Kim <melvynkim@gmail.com>
2026-02-08 15:10:36 -0600

    Update formula to v0.3.1

--- Formula/tokenuse.rb
+++ Formula/tokenuse.rb
@@ -1,30 +1,30 @@
 class Tokenuse < Formula
   desc "Track and analyze Claude Code usage"
   homepage "https://tokenuse.ai"
-  version "0.0.11"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/tokenuse/tokenuse/releases/download/v#{version}/tokenuse_#{version}_darwin_amd64.tar.gz"
-      sha256 "6335cc92f143c4b12be5c92e0d279ad8040d55902513e08448a9c7dc2060c54e"
+      sha256 "13939e1178875593e4088bd81c5bb6aa31611968a31416ba682825800413d556"
     end
 
     on_arm do
       url "https://github.com/tokenuse/tokenuse/releases/download/v#{version}/tokenuse_#{version}_darwin_arm64.tar.gz"
-      sha256 "30937924c9c533d05d6822ae999835b7d7e2d2256faaed09117220fe1445e7f3"
+      sha256 "b01b912344d98c731d47ab6db69685ac15f1dbf9820b7b80fd67419a98e0e33e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/tokenuse/tokenuse/releases/download/v#{version}/tokenuse_#{version}_linux_amd64.tar.gz"
-      sha256 "ccd575815b010ee7cdd88299bfda63394d5c3e9f1abea3b1d35916617a121c52"
+      sha256 "b5134a477a6140cf3bc4b43b7af4cee0e4abc74abaa1d533acf55707a9039329"
     end
 
     on_arm do
       url "https://github.com/tokenuse/tokenuse/releases/download/v#{version}/tokenuse_#{version}_linux_arm64.tar.gz"
-      sha256 "cc6939c4f3676dd1d2a40fd5bfddaa4ceed3c5cdb078d9739c39b7b16d256d24"
+      sha256 "5cddf627b6cae4b9ee8221c8a91aa9941a320fbfdc5f7c81bcb42149d7662d74"
     end
   end
 
